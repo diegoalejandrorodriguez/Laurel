0cbbe2bcf3611f1f9cd45ccc6c9964290ec4abc4
Miguel A Cortes O <miguel.cortes@mercadolibre.com.co>
2020-10-26 08:01:50 -0500

    Added android frontend project starter (#4)

---
+++ frontends/android/app/src/main/java/edu/co/javeriana/tais2020/laurel/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package edu.co.javeriana.tais2020.laurel.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
