c66c1a5ebb8bc99f6f1a0888bc379c18c72039b7
Miguel Cortes <miguel.cortes@mercadolibre.com.co>
2020-11-02 18:07:15 -0500

    Added navigation and base fragments

---
+++ frontends/android/app/src/main/java/edu/co/javeriana/tais2020/laurel/ui/menu/MenuViewModel.kt
@@ -0,0 +1,7 @@
+package edu.co.javeriana.tais2020.laurel.ui.menu
+
+import androidx.lifecycle.ViewModel
+
+class MenuViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ frontends/android/app/src/main/java/edu/co/javeriana/tais2020/laurel/ui/shopping/ShoppingViewModel.kt
@@ -0,0 +1,7 @@
+package edu.co.javeriana.tais2020.laurel.ui.shopping
+
+import androidx.lifecycle.ViewModel
+
+class ShoppingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
