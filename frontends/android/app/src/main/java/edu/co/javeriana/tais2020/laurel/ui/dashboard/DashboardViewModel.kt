68f0281bf53c7adff843a519e47da34464446d5e
Miguel Cortes <miguel.cortes@mercadolibre.com.co>
2020-11-02 16:28:06 -0500

    swtiched to kotlin for android frontend

---
+++ frontends/android/app/src/main/java/edu/co/javeriana/tais2020/laurel/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package edu.co.javeriana.tais2020.laurel.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ frontends/android/app/src/main/java/edu/co/javeriana/tais2020/laurel/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package edu.co.javeriana.tais2020.laurel.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
